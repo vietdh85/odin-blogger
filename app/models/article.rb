00b2fa8fe359c437903a95175564436181c51773
robinvh85 <hoangviet.vh@gmail.com>
2017-03-21 13:35:03 +0700

    Tagging feature completed

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,14 @@
 class Article < ApplicationRecord
 	has_many :comments
+	has_many :taggings
+	has_many :tags, through: :taggings
+
+	def tag_list
+		tags.join(", ")
+	end
+
+	def tag_list=(tags_string)
+		tag_names = tags_string.split(',').collect{ |s| s.strip.downcase }.uniq
+		self.tags = tag_names.collect { |name| Tag.find_or_create_by(name: name) } 
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ Rails.application.routes.draw do
   resources :articles do
   	resources :comments
   end
+  resources :tags
+  
 end
